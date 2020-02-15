1a6775d45e151ed9a6c54b54053d0a22a4f94149
Djonala <tristanjohanna@gmail.com>
2020-02-15 17:03:16 +0100

    Essai d'application du calendrier zimbra au fullcalendar - Johanna

--- src/Manager/CalendarManager.php
+++ src/Manager/CalendarManager.php
@@ -100,6 +100,80 @@ class CalendarManager
 
     }
 
+    /**
+     * Fonction qui selectionne les éléments nécessaires à l'affichage dans un fichier .json et qui renvoi un fichier json
+     * @param json $url
+     * @return array
+     * @throws Exception
+     */
+    public function creationEventsZimbraFC(json $url){
+        try {
+            $parsed_json = $this->parseJsonToPhpArray($url);
+        } catch (Exception $exception) {
+            echo $exception->getMessage();
+        }
+        try {
+            $ar_evenements = $this->selectArrayFileJson($parsed_json,'appt');
+        } catch (Exception $exception) {
+            echo $exception->getMessage();
+        }
 
+        $arrayEventZimbra = array();
+
+        foreach ($ar_evenements as $event) {
+            if (isset($event{'inv'}[0]{'comp'}[0]{'name'})) {
+                $titre = $event{'inv'}[0]{'comp'}[0]{'name'};
+            } else {
+                $titre = "titre non défini";
+            }
+
+            if (isset($event{'inv'}[0]{'comp'}[0]{'s'}[0]{'d'})) {
+                $dBegin = $event{'inv'}[0]{'comp'}[0]{'s'}[0]{'d'};
+                $dateHeureDebut = \DateTime::createFromFormat('Ymd\THis', $dBegin)->format('Y-m-d H:i:s');
+
+            } else {
+                throw new Exception('Date de debut non communiquée');
+            }
+
+            if (isset($event{'inv'}[0]{'comp'}[0]{'e'}[0]{'d'})) {
+                $dEnd = $event{'inv'}[0]{'comp'}[0]{'e'}[0]{'d'};
+                $dateHeureFin = \DateTime::createFromFormat('Ymd\THis', $dEnd)->format('Y-m-d H:i:s');
+            } else {
+                throw new Exception('Date de fin non communiquée');
+            }
+
+            $myObj->titre = $titre;
+            $myObj->start = $dBegin;
+            $myObj->end = $dEnd;
+            $arrayEventZimbra[] = $myObj;
+        }
+        $myJson= $this->parsePhpToJsonFile($arrayEventZimbra);
+        return $myJson;
+    }
+
+    public function parseJsonToPhpArray(string $url){
+        if(!file_get_contents($url)){
+            throw new Exception('Le lien URL est incorrect');
+        }
+        $json = file_get_contents($url); // Recupération du fichier json via l'url du calendrier passé en param
+        $parsed_json = json_decode($json, true);    // parse du fichier json en tableau PHP
+        return $parsed_json;
+    }
+
+    public function selectArrayFileJson(array $arrayPHP, string $selector){
+        if (!$arrayPHP){
+            throw new Exception('Le tableau est vide');
+        }
+        if (!$ar_select = $arrayPHP[$selector]) {
+            throw new Exception('Le selecteur n\'est pas retrouvé dans le tableau ');
+        }
+        $ar_select = $arrayPHP[$selector];
+        return $ar_select;
+    }
+
+    public function parsePhpToJsonFile(array $arPHP){
+        $arJSON = json_encode($arPHP);
+        return $arJSON;
+    }
 
 }
