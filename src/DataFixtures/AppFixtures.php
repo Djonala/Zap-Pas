aeb170ba2f392a9bfd4c464f15cf2bd6faf69a68
Djonala <tristanjohanna@gmail.com>
2020-02-21 12:10:51 +0100

    Ajout de securité sur mot de passe

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,9 +5,18 @@ namespace App\DataFixtures;
 use App\Entity\Users;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+
+    private $passwordEncoder;
+
+     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         $user = new Users();
@@ -15,7 +24,10 @@ class AppFixtures extends Fixture
         $user->setPrenom('Johanna');
         $user->setEmail('johanna.tristan@gmail.com');
         $user->setRoles(json_encode('ROLE_SUPER_ADMIN'));
-        $user->setPassword('motdepasse');
+        $user->setPassword($this->passwordEncoder->encodePassword(
+             $user,
+           'the_new_password'
+        ));
 
         $manager->persist($user);
 
