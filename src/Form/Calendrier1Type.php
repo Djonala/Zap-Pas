a1e280f73e302c456625d2baaf3e642e9fe50902
Djonala <tristanjohanna@gmail.com>
2020-03-19 23:49:39 +0100

    debug-Calendrier type

--- src/Form/Calendrier1Type.php
+++ src/Form/Calendrier1Type.php
@@ -3,7 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Calendrier;
+use App\Entity\Users;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Mapping\Entity;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +19,20 @@ class Calendrier1Type extends AbstractType
         $builder
             ->add('nom')
             ->add('url')
-            ->add('users')
+            ->add('users', EntityType::class, [
+                'class'=>Users::class,
+                'query_builder' => function(EntityRepository $er) {
+                    return $er->createQueryBuilder('u')
+//                        ->where('u.roles::text LIKE :role')
+                        ->orderBy('u.email', 'ASC');
+//                        ->setParameter('role', 'ROLE_ADMIN');
+                },
+
+                'choice_label'=>'prenom',
+                'multiple' => true,
+                'expanded' => true,
+            ])
+
         ;
     }
 
